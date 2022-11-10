40081c6b400a297f5c3a0dd1eb91aed35eb14122
Arya Acikgoz <aryaacikgoz@outlook.com>
2022-11-10 17:54:47 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import TextBox from "./components/TextBox";
+import Arrows from "./components/Arrows";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  // useState ile input ve output dillerini takip ediyorum.
+  const [inputLanguage, setInputLanguage] = useState("Turkish");
+  const [outputLanguage, setOutputLanguage] = useState("English");
+  // Modal'ı handle etmek için;
+  const [showModal, setShowModal] = useState(null);
+  // Languages
+  const [languages, setLanguages] = useState(null);
+
+  const getLanguages = async () => {
+    // RapidAPI Google Translate GET Languages
+    const options = {
+      method: "GET",
+      url: "https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+      headers: {
+        // "Accept-Encoding": "application/gzip",
+        "X-RapidAPI-Key": "66a1dce359msh19a1369f4144bb6p166940jsnf5aa3699069e",
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+        const langArray = Object.keys(response.data.data).map(
+          (key) => response.data.data[key]
+        );
+        setLanguages(langArray);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  console.log("languages", languages);
+
+  useEffect(() => {
+    getLanguages();
+  }, []);
+
+  const handleClick = () => {
+    // ortadaki çift yönlü oka tıklandığında input'u output olarak, output'u ise input'a set'lenmesini sağladım.
+    // böylelikle tıpkı google translate'teki gibi taraf değiştirecek.
+    setInputLanguage(outputLanguage);
+    setOutputLanguage(inputLanguage);
+  };
+
+  // console.log("showModal", showModal);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* eğer showModal boş ise; */}
+      {!showModal && (
+        <>
+          <TextBox
+            selectedLanguage={inputLanguage}
+            style="input"
+            setShowModal={setShowModal}
+          />
+          <div className="arrow-container" onClick={handleClick}>
+            <Arrows />
+          </div>
+          <TextBox
+            selectedLanguage={outputLanguage}
+            style="output"
+            setShowModal={setShowModal}
+          />
+        </>
+      )}
+      {showModal && <Modal setShowModal={setShowModal} />}
     </div>
   );
-}
+};
 
 export default App;
